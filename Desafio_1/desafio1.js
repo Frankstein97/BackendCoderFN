aea012cc6fd403ee9bcd136e37eb9fab953af603
Frankstein97 <francoivannicoletti@gmail.com>
2023-09-19 21:23:22 -0300

    Desafio 1 con validacion campos obligatorios

--- Desafio_1/desafio1.js
+++ Desafio_1/desafio1.js
@@ -1,4 +1,4 @@
-console.log("Este es el desafío 1, porfavor que este bien ahora T_T");
+console.log("Este es el desafío 1, otra vez T_T");
 
 class ProductManager {
   constructor() {
@@ -12,48 +12,53 @@ class ProductManager {
 
   // Método para agregar productos nuevos.
   addProducts(title, description, price, thumbnail, code, stock) {
+    // Validar que todos los campos sean obligatorios
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.log("Por favor, complete todos los campos");
+      return;
+    }
+  
+    // Verificar si el código del producto ya existe
     const isCodeUnique = this.products.every((product) => product.code !== code);
     if (!isCodeUnique) {
       console.log(`El código ${code} del producto ya está en uso.`);
-  return;
-
-  }
-    // Si no encontramos un producto con el mismo código (índice es -1), lo agregamos.
-    if (isCodeUnique) {
-      this.products.push({
-        id: this.products.length + 1,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      });
+      return;
     }
+  
+    // Agregar el producto si todo está bien
+    this.products.push({
+      id: this.products.length + 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    });
   }
 
-  // Método para obtener un producto por su ID.
   getProductsById = (id) => {
     // Usamos find para buscar el producto por su ID.
     const product = this.products.find((product) => product.id === id);
-    
-    // Si no encontramos el producto, devolvemos un mensaje de error.
-    return !product ? `El ID ${id} no fue encontrado` : product;
-  }
+    // Si no encontramos el producto, registramos un mensaje de error.
+    if (!product) {
+      console.log(`El ID ${id} no fue encontrado`);
+      return null;
+    }
+    return product;
+  };
 }
-
 
 ////////////////////////TESTING////////////////////////
 let productManager = new ProductManager();
 
 // >>>>
-productManager.addProducts("title", "description", 11 ,"thumbnail","code", 10);
-productManager.addProducts("title", "description", 11 ,"thumbnail","code2", 10);
-productManager.addProducts("title", "description", 11 ,"thumbnail","code2", 10);
-productManager.addProducts("title", "description", 11 ,"thumbnail","121212", 10);
-productManager.addProducts("title", "description", 11 ,"thumbnail","121212", 10);
-console.log (productManager.getProducts())
+productManager.addProducts("title", "description", 11, "thumbnail", "code", 10);
+productManager.addProducts("title","description",11,"thumbnail", "code2", 10);
+productManager.addProducts("title","description",11,"thumbnail", "code2", 10);
+productManager.addProducts("title", "description", 11, "thumbnail");
+console.log(productManager.getProducts());
 
 // >>>>
-console.log (productManager.getProductsById(1))
-console.log (productManager.getProductsById(14))
+console.log(productManager.getProductsById(1));
+console.log(productManager.getProductsById(14));
